ebbff7487f9145237a413b436808b76a9db74e62
a5213803 <45652060+a5213803@users.noreply.github.com>
2018-12-13 15:47:28 +0800

    Update ex11.ino

--- ex11.ino
+++ ex11.ino
@@ -1,89 +1,44 @@
 #include <LWiFi.h>
-#include <WiFiClient.h>
 #include "MCS.h"
 
-// Assign AP ssid / password here
-#define _SSID "your_ssid"
-#define _KEY  "your_password"
+char _lwifi_ssid[] = "ws";
+char _lwifi_pass[] = "0989787226";
 
-// Assign device id / key of your test device
-MCSDevice mcs("your_device_id", "your_device_key");
+MCSDevice mcs("DLfqjdFL", "6xMHxN200uRCLj7f");
+MCSControllerOnOff MySwitch("MySwitch");
 
-// Assign channel id 
-// The test device should have 2 channel
-// the first channel should be "Controller" - "On/Off"
-// the secord channel should be "Display" - "On/Off"
-MCSControllerOnOff led("your_channel1_id");
-MCSDisplayOnOff    remote("your_channel2_id");
-
-#define LED_PIN 7
-
-void setup() {
-  // setup Serial output at 9600
+void setup()
+{
   Serial.begin(9600);
 
-  // setup LED/Button pin
-  pinMode(LED_PIN, OUTPUT);
-
-  // setup Wifi connection
-  while(WL_CONNECTED != WiFi.status())
-  {
-    Serial.print("WiFi.begin(");
-    Serial.print(_SSID);
-    Serial.print(",");
-    Serial.print(_KEY);
-    Serial.println(")...");
-    WiFi.begin(_SSID, _KEY);
-  }
-  Serial.println("WiFi connected !!");
-
-  // setup MCS connection
-  mcs.addChannel(led);
-  mcs.addChannel(remote);
-  while(!mcs.connected())
-  {
-    Serial.println("MCS.connect()...");
-    mcs.connect();
-  }
-  Serial.println("MCS connected !!");
-
-  // read LED value from MCS server
-  while(!led.valid())
-  {
-    Serial.println("read LED value from MCS...");
-    led.value();
-  }
-  Serial.print("done, LED value = ");
-  Serial.println(led.value());
-  digitalWrite(LED_PIN, led.value() ? HIGH : LOW);
+  mcs.addChannel(MySwitch);
+  Serial.println("Wi-Fi 開始連線");
+  while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) { delay(1000); }
+  Serial.println("Wi-Fi 連線成功");
+  while(!mcs.connected()) { mcs.connect(); }
+  Serial.println("MCS 連線成功");
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // call process() to allow background processing, add timeout to avoid high cpu usage
-  Serial.print("process(");
-  Serial.print(millis());
-  Serial.println(")");
+void loop()
+{
+  while (!mcs.connected()) {
+    mcs.connect();
+    if (mcs.connected()) { Serial.println("MCS 已重新連線"); }
+  }
   mcs.process(100);
-  
-  // updated flag will be cleared in process(), user must check it after process() call.
-  if(led.updated())
-  {
-    Serial.print("LED updated, new value = ");
-    Serial.println(led.value());
-    digitalWrite(LED_PIN, led.value() ? HIGH : LOW);
-    if(!remote.set(led.value()))
-    {
-      Serial.print("Failed to update remote");
-      Serial.println(remote.value());
+
+  if (MySwitch.updated()) {
+    Serial.print("控制通道更新 :");
+    Serial.println(MySwitch.value());
+
+    if (MySwitch.value()) {
+      digitalWrite(LED_BUILTIN, HIGH);
+
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
     }
   }
-  
-  // check if need to re-connect
-  while(!mcs.connected())
-  {
-    Serial.println("re-connect to MCS...");
-    mcs.connect();
-    if(mcs.connected())
-      Serial.println("MCS connected !!");
-  }
+  delay(1000);
 }
